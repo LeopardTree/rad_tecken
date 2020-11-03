9ad512079b8c11361a711b347df1dcecdf45fbe9
Ludvig Viveland <ludvigvive@gmail.com>
2020-11-03 16:31:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rad_tecken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Reads this textfile: ");
+            //Console.WriteLine("this textfile: C:\\Users\\ludvi\\progmet\\textfilewithtext.txt");
+            string path = "C:\\Users\\ludvi\\progmet\\textfilewithtext.txt";
+            string[] lines = File.ReadAllLines(@path);
+            int rows = 0;
+            int countChars = 0;
+            int countWords = 0;
+            Console.WriteLine($"contents of {path}: ");
+            Console.WriteLine("");
+            foreach (string line in lines)
+            {
+                string[] word = line.Split(' ');
+                countWords = countWords + word.Length;
+                
+                foreach (char chr in line)
+                {
+                    
+                    
+                    if(!char.IsWhiteSpace(chr))
+                    {
+                        countChars++;
+                    }
+
+                }
+                rows++;
+                Console.WriteLine("\t" + line);
+            }
+            Console.WriteLine($"Det är {countWords} ord i textfilen");
+            Console.WriteLine($"Det är {rows} rader i textfilen" );
+            Console.WriteLine($"Det är {countChars} tecken i textfilen");
+            Console.ReadKey();
+        }
+    }
+}
